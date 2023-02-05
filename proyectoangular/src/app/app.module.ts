29d4a626e44bd038f2e8535be173e60746eae696
SebastianRodriguez02 <sebastian3696@gmail.com>
2023-02-05 00:33:52 -0500

    Angular

--- proyectoangular/src/app/app.module.ts
+++ proyectoangular/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Tienda1Component } from './components/tienda1/tienda1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    Tienda1Component
   ],
   imports: [
     BrowserModule,
---
+++ proyectoangular/src/app/components/tienda1/tienda1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tienda1',
+  templateUrl: './tienda1.component.html',
+  styleUrls: ['./tienda1.component.css']
+})
+export class Tienda1Component {
+
+}
